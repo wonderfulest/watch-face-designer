c7a364c81b9b7024bbfa67698c173f5bde9a728a
muyids <289869876@qq.com>
2025-04-06 11:27:23 +0800

    core

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -15,8 +15,7 @@ const axiosInstance = axios.create({
 // 请求拦截器
 axiosInstance.interceptors.request.use(
   (config) => {
-    // const token = localStorage.getItem('token')
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNzQzNTc2Nzk1LCJleHAiOjE3NDYxNjg3OTV9.FumIaj9F6QKfbFQl3P0nZvdWg_IxsKdwpLKWwUP9Obo"
+    const token = localStorage.getItem('token')
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
